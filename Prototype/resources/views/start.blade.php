d9ba5965572b50c6023b064d6a37c9c3c75df7eb
Sebi2202 <sebastian.herberth@yahoo.com>
2017-11-04 17:49:49 +0100

    test

--- Prototype/resources/views/start.blade.php
+++ Prototype/resources/views/start.blade.php
@@ -5,5 +5,8 @@
     </head>
     <body>
         <h1>Startseite!</h1>
+        <input type"textfield" value="Text eingeben!..">
+        <input type"textfield" value="Text eingeben!..">
+        <input type="submit">
     </body>
 </html>
