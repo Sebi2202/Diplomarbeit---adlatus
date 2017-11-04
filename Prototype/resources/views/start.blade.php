75e4ba1c645d33a64bc51b94da83cfca3abb1cbc
Benni <benjamin.herberth@yahoo.de>
2017-11-04 17:10:31 +0100

    Startseite
    Man kann auf die Startseite draufgehen indem man auf die URL 'prototype.dev/start' geht

---
+++ Prototype/resources/views/start.blade.php
@@ -0,0 +1,9 @@
+<!doctype html>
+<html>
+    <head>
+        <title>Startseite</title>
+    </head>
+    <body>
+        <h1>Startseite</h1>
+    </body>
+</html>
--- Prototype/routes/web.php
+++ Prototype/routes/web.php
@@ -14,3 +14,8 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/start', function () {
+    return view('start');
+});
+
