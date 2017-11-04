3034aa96d4e969504bb41e2f871ed73f8b0cdfc4
Sebi2202 <sebastian.herberth@yahoo.com>
2017-11-04 19:47:40 +0100

    Little Update

--- Prototype/app/Http/Controllers/PagesController.php
+++ Prototype/app/Http/Controllers/PagesController.php
@@ -7,12 +7,12 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function start() {
-        $title = 'Willkommen zur Startseite';
+        $title = 'Willkommen zur Startseite!';
         return view('pages/start', compact('title'));
     }
 
     public function app() {
-        $title = 'Willkommen zur Appseite';
+        $title = 'Willkommen zur Appseite!';
         return view('pages/app', compact('title'));
     }
 }
