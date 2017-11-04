62f6a1eab57360e462a16d6efe80086b8eaba37c
Sebi2202 <sebastian.herberth@yahoo.com>
2017-11-04 19:52:06 +0100

    Schönere Buttons für Appseite

--- Prototype/resources/views/pages/app.blade.php
+++ Prototype/resources/views/pages/app.blade.php
@@ -16,13 +16,29 @@
 			.agree {
 				position:relative;
 				bottom:50px;
-				background-color:lightgreen;
+				background-color:#4CAF50;
+				border:none;
+				color:white;
+				padding:15px 32px;
+				text-align:center;
+				text-decoration:none;
+				display:inline-block;
+				font-size:16px;
+				width:120px;
 			}
 			
 			.disagree {
 				position:relative;
 				bottom:50px;
 				background-color:red;
+				border:none;
+				color:white;
+				padding:15px 32px;
+				text-align:center;
+				text-decoration:none;
+				display:inline-block;
+				font-size:16px;
+				width:auto;
 			}
         </style>
     </head>
