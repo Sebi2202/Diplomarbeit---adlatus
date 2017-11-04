fc80d0cf420c9fb8f498f61a344fda551b696ce8
Benni <benjamin.herberth@yahoo.de>
2017-11-04 21:26:52 +0100

    Migration create tasks

--- Prototype/database/migrations/2017_11_04_202220_create_tasks_table.php
+++ Prototype/database/migrations/2017_11_04_202220_create_tasks_table.php
@@ -15,6 +15,8 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('Beschreibung');
+            $table->boolean('false');
             $table->timestamps();
         });
     }
