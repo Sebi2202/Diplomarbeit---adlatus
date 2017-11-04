d741c6c682d3d270b746731c59e2639fe3eaa41a
Sebi2202 <sebastian.herberth@yahoo.com>
2017-11-04 21:06:25 +0100

    CSS changes Startseite

--- Prototype/resources/views/pages/start.blade.php
+++ Prototype/resources/views/pages/start.blade.php
@@ -6,12 +6,32 @@
                 text-align:center;
 				font-family: 'Roboto', sans-serif;
             }
+
+            .field {
+                padding: 20px;
+                box-sizing: border-box;
+            }
+
+            .sbm {
+               
+				background-color:#ccc;
+    			border:none;
+    			color:white;
+    			padding:6px 12px;
+    			text-align:center;
+    			text-decoration: none;
+    			display:inline-block;
+    			font-size:14px;
+				width:80.3px;
+				margin-left:10px;
+			
+            }
         </style>
     </head>
     <body>
         <div>
             <h1>{{$title}}!</h1>
-            <input type="text" size="40" placeholder="Text eingeben!..">
-            <input type="submit" value="Submit!">
+            <input type="text" class="field" size="40">
+            <input type="submit" class="sbm" value="Submit!">
         </div>
 @endsection
