5ba0004dcdaa1d256d2c226d9164cd51eb7183b4
Benni <benjamin.herberth@yahoo.de>
2017-11-15 19:00:58 +0100

    Layout erstellt

--- Umsetzung/Adlatus_Laravel/resources/views/layout/layout.blade.php
+++ Umsetzung/Adlatus_Laravel/resources/views/layout/layout.blade.php
@@ -4,10 +4,7 @@
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-
-        <title>Startseite</title>
-    </head>
-    <body>
-        <h2>Es klappt</h2>
+        
+        @yield('content')
     </body>
 </html>
---
+++ Umsetzung/Adlatus_Laravel/resources/views/pages/welcome.blade.php
@@ -0,0 +1,11 @@
+ @extends('layout/layout')
+
+ @section('content')
+    <style>
+
+    </style>
+    <title>Startseite</title>
+    </head>
+    <body>
+        <h2>Es klappt</h2>
+@endsection
