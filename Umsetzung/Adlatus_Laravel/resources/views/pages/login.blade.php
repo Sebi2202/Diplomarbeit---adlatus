413c25892e393fb617c80722483e4416ea6317a6
Sebi2202 <sebastian.herberth@yahoo.com>
2017-11-19 13:30:12 +0100

    UPDATE Resp. D

--- Umsetzung/Adlatus_Laravel/resources/views/pages/login.blade.php
+++ Umsetzung/Adlatus_Laravel/resources/views/pages/login.blade.php
@@ -133,6 +133,11 @@
 
         @media screen and (max-height:525px) { footer { display:none; } }
         @media screen and (max-width:770px) and (max-height:582px) { footer { display:none; } }
+        @media screen and (max-width:508px) {
+            section { width:255px; }
+            .fm { width:178px; }
+            .lg { width:137px; }
+        }
 
         @media screen and (max-width:770px) {.fm {width:80%;} .lg { float:left; } .last_text { float:left; } section { height:320px; } }
         
