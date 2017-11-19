c16e0a09142559f1eebf832d4893aabafd2da5fb
Benni <benjamin.herberth@yahoo.de>
2017-11-19 12:32:22 +0100

    lil change of structure

--- Umsetzung/Adlatus_Laravel/routes/web.php
+++ Umsetzung/Adlatus_Laravel/routes/web.php
@@ -15,7 +15,7 @@ Route::get('/', 'PagesController@showStart');
 Route::get('/login', 'PagesController@showLogin');
 Route::get('/registrierung', 'TherapeutController@create');
 Route::get('/error', 'PagesController@error');
-Route::post('/dashboard', 'LoginController@login');
 
+Route::post('/dashboard', 'LoginController@login');
 Route::post('/registrierung', 'TherapeutController@store');
 
