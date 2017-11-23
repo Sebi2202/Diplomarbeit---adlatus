4197c246cd37fc59cd4a523f4a3b46cbc464ba50
Benjey22 <benjamin.herberth@yahoo.de>
2017-11-23 16:31:20 +0100

    soz to char in patients

--- Umsetzung/Adlatus_Laravel/database/migrations/2017_11_20_193025_create_table_patients.php
+++ Umsetzung/Adlatus_Laravel/database/migrations/2017_11_20_193025_create_table_patients.php
@@ -14,7 +14,7 @@ class CreateTablePatients extends Migration
     public function up()
     {
         Schema::create('patients', function(Blueprint $table) {
-            $table->bigInteger('sozNr')->primary();
+            $table->char('sozNr', 20)->primary();
             $table->string('vorname');
             $table->string('nachname');
             $table->string('email')->nullable();
