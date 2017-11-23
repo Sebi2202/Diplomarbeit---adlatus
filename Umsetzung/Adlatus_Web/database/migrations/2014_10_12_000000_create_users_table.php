6115dbecfcb4d9ae7604166e3ad729bf988aa935
Benjey22 <benjamin.herberth@yahoo.de>
2017-11-23 17:31:48 +0100

    neues projekt erstellt

---
+++ Umsetzung/Adlatus_Web/database/migrations/2014_10_12_000000_create_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->increments('id');
+            $table->char('sozNr', 20)->unique();
+            $table->string('vorname');
+            $table->string('nachname');
+            $table->string('email');
+            $table->string('password');
+            $table->integer('role_id');
+            $table->char('therapeut_sozNr', 20)->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+            //$table->foreign('role_id')->references('id')->on('roles');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
