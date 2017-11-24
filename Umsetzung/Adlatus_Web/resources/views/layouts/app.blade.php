057f6bc7fc47b3458e88fe6c21bdb2b2d9cd0778
Benni <benjamin.herberth@yahoo.de>
2017-11-24 14:08:40 +0100

    layouts.app changes

--- Umsetzung/Adlatus_Web/resources/views/layouts/app.blade.php
+++ Umsetzung/Adlatus_Web/resources/views/layouts/app.blade.php
@@ -8,47 +8,28 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <!--
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-default navbar-static-top">
             <div class="container">
-                <div class="navbar-header">
-
-                    <!-- Collapsed Hamburger -->
-                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse" aria-expanded="false">
-                        <span class="sr-only">Toggle Navigation</span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-
-                    <!-- Branding Image -->
-                    <a class="navbar-brand" href="{{ url('/') }}">
-                        {{ config('app.name', 'Laravel') }}
-                    </a>
-                </div>
+                
 
                 <div class="collapse navbar-collapse" id="app-navbar-collapse">
-                    <!-- Left Side Of Navbar -->
-                    <ul class="nav navbar-nav">
-                        &nbsp;
-                    </ul>
+                    
+                    
 
-                    <!-- Right Side Of Navbar -->
+                    
                     <ul class="nav navbar-nav navbar-right">
-                        <!-- Authentication Links -->
+                        
                         @guest
                             <li><a href="{{ route('login') }}">Login</a></li>
-                            <li><a href="{{ route('register') }}">Register</a></li>
+                            <li><a href="{{ route('register') }}">Registrierung</a></li>
                         @else
                             <li class="dropdown">
                                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false" aria-haspopup="true">
-                                    {{ Auth::user()->vorname }} <span class="caret"></span>
+                                    {{ Auth::user()->vorname }} {{ Auth::user()->nachname }} <span class="caret"></span>
                                 </a>
 
                                 <ul class="dropdown-menu">
@@ -69,7 +50,7 @@
                     </ul>
                 </div>
             </div>
-        </nav>
+        </nav>-->
 
         @yield('content')
     </div>
