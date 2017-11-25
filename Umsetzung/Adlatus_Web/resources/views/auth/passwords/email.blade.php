44dcbbf83224bf03c0c42e9ca50edc0ef8cabde4
Benni <benjamin.herberth@yahoo.de>
2017-11-25 01:50:27 +0100

    comment deleted

--- Umsetzung/Adlatus_Web/resources/views/auth/passwords/email.blade.php
+++ Umsetzung/Adlatus_Web/resources/views/auth/passwords/email.blade.php
@@ -20,35 +20,7 @@
                     {{ Form::email('email', '', ['class' => '', 'placeholder' => 'E-Mail']) }} 
                     <br>
                     {{ Form::submit('Passwort zurücksetzen', ['class' => '']) }}
-                    <!--
-                    <form class="form-horizontal" method="POST" action="{{ route('password.email') }}">
-                        {{ csrf_field() }}
-
-                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
-                            
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="help-block">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
-                                    <a href="{{route('sendEmail')}}" style="color:black">Send Password Reset Link</a>
-                                </button>
-                            </div>
-                        </div> 
-                    </form>
-                    {!! Form::close() !!}
-                    -->
+                    
                 </div>
             </div>
         </div>
