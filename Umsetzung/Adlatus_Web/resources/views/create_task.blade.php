120f465ed8c493d9090cb5203b4dd7c32d8bbbba
Benni <benjamin.herberth@yahoo.de>
2018-01-14 12:54:32 +0100

    update little changes

--- Umsetzung/Adlatus_Web/resources/views/create_task.blade.php
+++ Umsetzung/Adlatus_Web/resources/views/create_task.blade.php
@@ -29,33 +29,28 @@
     <section>
         <div id="really">
                 <div class="">
-                    <form action="#" method="post">
-                    <a href="#really" class="" name="activity" value="Arzttermin">Insert Logo</a><label>Arzttermin</label><br>
-                    <a href="#really" class="" name="activity" value="Körperpflege">Insert Logo</a><label>Körperpflege</label><br>
-                    <a href="#really" class="" name="activity" value="Einkaufen">Insert Logo</a><label>Einkaufen</label><br>
-                    <a href="#really" class="" name="activity" value="Anrufe">Insert Logo</a><label>Anrufe</label><br>
-                    <a href="#really" class="" name="activity" value="Trinken">Insert Logo</a><label>Trinken</label><br>
-                    <a href="#really" class="" name="activity" value="Blutdruckmessen">Insert Logo</a><label>Blutdruckmessen</label><br>
-                    <a href="#really" class="" name="activity" value="Medikamente">Insert Logo</a><label>Medikamente</label><br>
-                    <a href="#really" class="" name="activity" value="Blumen gießen">Insert Logo</a><label>Blumen gießen</label><br>
-                    <a href="#really" class="" name="activity" value="Essen">Insert Logo</a><label>Essen</label><br>
-                    <a href="#really" class="" name="activity" value="Wäsche waschen">Insert Logo</a><label>Wäsche waschen</label><br>
-                    <a href="#really" class="" name="activity" value="Bewegung">Insert Logo</a><label>Bewegung</label><br>
-                    <a href="#really" class="" name="activity" value="Aufstehen">Insert Logo</a><label>Aufstehen</label><br>
-                    <a href="#really" class="" name="activity" value="Schlafenszeit">Insert Logo</a><label>Schlafenszeit</label><br>
-                    <a href="#really" class="" name="activity" value="Erholungszeit">Insert Logo</a><label>Erholungszeit</label><br>
-                    <a href="#really" class="" name="activity" value="Haustiere versorgen">Insert Logo</a><label>Haustiere versorgen</label><br>
-                    <a href="#really" class="" name="activity" value="Zähneputzen">Insert Logo</a><label>Zähneputzen</label><br>
-                    <a href="#really" class="" name="activity" value="Gedächtnistraining">Insert Logo</a><label>Gedächtnistraining</label>
-                    </form>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Arzttermin</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Körperpflege</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Einkaufen</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Anrufe</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Trinken</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Blutdruckmessen</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Medikamente</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Blumen gießen</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Essen</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Wäsche waschen</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Bewegung</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Aufstehen</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Schlafenszeit</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Erholungszeit</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Haustiere versorgen</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Zähneputzen</label><br>
+                    <a href="#really"><img src="/icons/medic.png"></a><label>Gedächtnistraining</label>
                 </div>
+
                 <div class="sure">
                     {!! Form::open(['action' => ['TaskController@store', $user->id], 'method' => 'POST']) !!}
-                    <?php 
-                        if(isset($_POST['activity'])) {
-                            echo $_POST['activity'];
-                        }
-                    ?>
+                    
                     <h2><label id="label1">Test</label></h2>
                     {{ Form::hidden('title', 'Test')}}
                     {{ Form::hidden('activitynr', '5') }}
@@ -68,6 +63,7 @@
                         <a class="" href="#">Abbrechen</a>
                     </div>
                 </div>
+                
             </div>
     </section>
 </body>
