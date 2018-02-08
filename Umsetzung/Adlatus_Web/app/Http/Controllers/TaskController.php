2181f190425c2d16e0a0307478e0b88c73529851
Benjey22 <benjamin.herberth@yahoo.de>
2018-02-08 13:38:08 +0100

    update store function

--- Umsetzung/Adlatus_Web/app/Http/Controllers/TaskController.php
+++ Umsetzung/Adlatus_Web/app/Http/Controllers/TaskController.php
@@ -50,26 +50,24 @@ class TaskController extends Controller
         $pieces = explode("/", url()->previous());
         $date = $pieces[sizeof($pieces)-1];
 
-        $tsks = Task::where('fk_userid', $id);
+        $tsks = Task::all()->where('fk_userid', $id);
 
         foreach ($tsks as $tsk) {
             if($tsk->start == $date . " " . $request->input('date') . ":00") {
                 return redirect('/dashboard/patient/calendar/' . $id . '/' . $date)->with('Fail', 'Task already exist on this time');
             }
-            else {
-                $task = new Task();
-                $task->fk_userid = $id;
-                $task->fk_activityid = $request->input('activitynr');
-                $task->start = $date . " " . $request->input('date') . ":00";
-                $task->title = $request->input('title');
-                $task->confirmed = 0;
-                $task->nachricht = $request->input('message');
-                $task->link = $request->input('link');
-        
-                $task->save();
-            }
         }
         
+        $task = new Task();
+        $task->fk_userid = $id;
+        $task->fk_activityid = $request->input('activitynr');
+        $task->start = $date . " " . $request->input('date') . ":00";
+        $task->title = $request->input('title');
+        $task->confirmed = 0;
+        $task->nachricht = $request->input('message');
+        $task->link = $request->input('link');
+        
+        $task->save();
         
         return redirect('/dashboard/patient/calendar/' . $id . '/' . $date)->with('Success', 'Task created');
         
